aae4d08fe899773c68be727000ab3a05c2f6ee01
ogiwarat <ogiwarat@mamezou.com>
2021-03-16 00:00:07 +0900

    change artifact name

--- 5th_cicd/cicd-sample/deployment/scripts/start_server.sh
+++ 5th_cicd/cicd-sample/deployment/scripts/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 cd /home/ec2-user/cicdSampleApp
-java -Dserver.port=80 -Dserver.sockets.0.port=443 -jar cicd-sample.jar > /dev/null 2>&1 < /dev/null &
+java -Dserver.port=80 -Dserver.sockets.0.port=443 -jar cicd-sample-main.jar > /dev/null 2>&1 < /dev/null &
 echo "[cicdSampleAppliction]START SEVER"
